19f73bd8b48dc335bf8aa357e810db85b966132f
Deniz Tetik <deniztetik31@gmail.com>
2016-05-24 20:15:35 -0400

    added basic styling

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     var min = min || 0;
     return function() {
       var index = streams.home.length - 1;
-      for (var i = index; i >= min; i--) {
+      for (var i = min; i < index; i++) {
         var tweet = streams.home[i];
         var $tweet = $('<div></div>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message + ' at ' + tweet.created_at);
+        $tweet.html("<user>@" + tweet.user + ":</user> " + tweet.message + " at " + tweet.created_at);
         $tweets.prepend($tweet);
       }
     min = index;
@@ -16,5 +16,6 @@ $(document).ready(function(){
   }
   var stream = showStream();
   stream();
-  $('button').on('click', stream);
+  setInterval(stream, 3000);
+  $('.btn').on('click', stream);
 });
